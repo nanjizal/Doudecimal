70213bb79b2e26ebc346513ea4e2532b645cdf74
nanjizal <nitsuj@nanjizal.net>
2022-11-16 10:45:51 +0000

    using UNorm for ARGB_f

--- src/doudecimal/ARGB_f.hx
+++ src/doudecimal/ARGB_f.hx
@@ -1,12 +1,12 @@
 package doudecimal;
-
+import doudecimal.UNorm;
 @:structInit
 class ARGB_f {
-    public var a: Float;
-    public var r: Float;
-    public var g: Float;
-    public var b: Float;
-    public inline function new( a: Float, r: Float, g: Float, b: Float ){
+    public var a: UNorm;
+    public var r: UNorm;
+    public var g: UNorm;
+    public var b: UNorm;
+    public inline function new( a: UNorm, r: UNorm, g: UNorm, b: UNorm ){
         this.a = a;
         this.r = r;
         this.g = g;
